fe70fddff22c52caf274ac19ff68ebdde491b1f0
LunifyDev <noreply@lunify.xyz>
2025-03-08 20:09:30 +0100

    Show version in RPC API, even on restricted mode

--- src/rpc/core_rpc_server.cpp
+++ src/rpc/core_rpc_server.cpp
@@ -605,7 +605,7 @@ bool core_rpc_server::on_get_info(const COMMAND_RPC_GET_INFO::request &req,
   if (restricted)
     res.database_size = round_up(res.database_size, 5ull * 1024 * 1024 * 1024);
   res.update_available = restricted ? false : m_core.is_update_available();
-  res.version = restricted ? "" : lunify_VERSION_FULL;
+  res.version = lunify_VERSION_FULL;
   res.synchronized = check_core_ready();
   res.busy_syncing = m_p2p.get_payload_object().is_busy_syncing();
   res.restricted = restricted;
